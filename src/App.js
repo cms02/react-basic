0d13b875a183d92f5bcf4d499ee4aff69a88ab1b
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-10 17:52:22 +0900

    styled-components 사용전

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-// useRef(디자인)
-// dom을 변경할 때 사용
+import './App.css';
 
-import { createRef, useRef, useState } from 'react';
+const a = {
+  backgroundColor: 'red',
+};
 
 function App() {
-  const myRef = useRef(null);
-  const [list, setList] = useState([
-    { id: 1, name: '길동' },
-    { id: 2, name: '꺽정' },
-  ]);
-  const myRefs = Array.from({ length: list.length }).map(() => createRef());
-
   return (
     <div>
-      <button
-        onClick={() => {
-          console.log(myRef);
-          console.log(myRef.current);
-          // myRef.current.style.backgroundColor = 'red';
-          myRefs[0].current.style.backgroundColor = 'red';
-          myRefs[1].current.style.backgroundColor = 'blue';
-        }}
-      >
-        색 변경
-      </button>
-      <div ref={myRef}>박스</div>
-      {list.map((user, index) => (
-        <h1 ref={myRefs[index]}>{user.name}</h1>
-      ))}
+      <div style={a}>안녕</div>
+      <div className="box-style">헬로</div>
     </div>
   );
 }
