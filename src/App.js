ed163e23b56f4eb6f214b0b1251f10572487cf5b
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-10 15:26:57 +0900

    concat, filter, map, slice, spread, useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 //0. React엔진 - 데이터변경감지에서 UI그려주는 엔진!
@@ -14,16 +15,43 @@ import './App.css';
 // - 라이브러리 사용(부트스트랩, component-styled)
 
 function App() {
-  let list = [1, 2, 3];
+  console.log('App 실행됨');
 
-  let number = 1;
+  const [num, setNum] = useState(5);
 
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '김길동' },
+    { id: 3, name: '황길동' },
+    { id: 4, name: '박길동' },
+  ];
+
+  //다운로드 받음
+  const [users, setUsers] = useState(sample); //레퍼런스 변경되어야 동장!!
+  const download = () => {
+    //fetch().then().then();
+    //기존 데이터 세팅
+
+    //기존 데이터 세팅
+    // sample.push({ id: 5, name: '조자룡' });
+    // console.log(sample);
+    // setUsers(sample);
+
+    // const a = sample.concat({ id: 5, name: '조자룡' });
+    console.log(sample);
+    setUsers([...sample, { id: num, name: '조자룡' }]);
+    setNum(num + 1);
+  };
+
+  //랜더링 시점 = 상태값 변경
   return (
     <div>
-      <input type="text" />
       <div>
-        {list.map((n) => (
-          <h1>{n}</h1>
+        <button onClick={download}>다운로드</button>
+        {users.map((u) => (
+          <h1>
+            {u.id},{u.name}
+          </h1>
         ))}
       </div>
     </div>
