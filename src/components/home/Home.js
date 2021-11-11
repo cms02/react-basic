9c88f56bb320e7891986a11ff765824799a3cbec
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-11 10:44:43 +0900

    styled component props 패싱

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import { num } from '../../Sub';
 
 // Function 방식(👍)
 // class 방식
+
+let StyledDeleteButton = styled.button`
+  color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
+`;
+
+//부모로부터 받아온 어떤 데이터를 가지고 스타일링을 동적으로 할것이라면?
 const Home = (props) => {
   //   const boards = props.boards;
   //👇
   //구조분할 할당
-  const { boards, setBoards, setNumber, number } = props;
+  const { boards, setBoards, setNumber, number, user } = props;
 
   //                👆 함수를 자식한테 passing
 
@@ -15,7 +22,9 @@ const Home = (props) => {
     <div>
       <h1>홈 : {number} </h1>
       <button onClick={() => setNumber(number + 1)}>번호증가</button>
-      <button onClick={() => setBoards([])}>전체 삭제</button>
+      <StyledDeleteButton user={user} onClick={() => setBoards([])}>
+        전체 삭제
+      </StyledDeleteButton>
       {boards.map((board) => (
         <h3>
           제목: {board.title} 내용: {board.content}
